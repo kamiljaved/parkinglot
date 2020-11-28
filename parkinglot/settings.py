86c3647f013e63117b3b58e35153e261b0925cb0
kamiljaved98 <40760774+kamiljaved98@users.noreply.github.com>
2020-11-29 00:50:13 +0500

    Update settings.py

--- parkinglot/settings.py
+++ parkinglot/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nk4p=!d49n3)-ypv=6aw+f*wzwl*h@wx@g^%hp^_)a^&0c2!*z'
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
